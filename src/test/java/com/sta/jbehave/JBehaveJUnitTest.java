1e1494be15011b37a70b6667fd1b8992678120bf
Matt DeFano <matt.defano@gmail.com>
2014-01-29 14:48:49 -0600

    Added comments.

--- src/test/java/com/sta/jbehave/JBehaveJUnitTest.java
+++ src/test/java/com/sta/jbehave/JBehaveJUnitTest.java
@@ -10,13 +10,30 @@ import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.Ignore;
 
+/**
+ * This class provides "boilerplate" functionality to wrap JBehave in JUnit. Extend a steps class
+ * with this abstract base class to make the steps class look like a JUnit. Then, you can execute
+ * your steps class using any toolset that supports JUnit (for example, in Eclipse, you could right-click
+ * the steps class and choose Run As-->JUnit Test).
+ * 
+ * @author mattd
+ */
 @Ignore
 public abstract class JBehaveJUnitTest extends JUnitStory {
 
+	/**
+	 * All JBehave steps classes (that is, those which provide methods annotated with given/when/then) must
+	 * implement this method. This enables us--the superclass--to register our subclass as the candidate steps 
+	 * used for behavioral testing. (Essentially, this technique works around Java's inability to reference a 
+	 * subclass from within a superclass.)
+	 * 
+	 * @return An instance of the class implementing the given/when/then methods for a story; typically, 'this'.	 
+	 */	
 	public abstract Object getStepsClass();
 
 	/**
-	 * Returns a default JBehave configuration for each run.
+	 * Returns a default JBehave configuration (this configuration will apply to all steps classes
+	 * that extend us).
 	 */
 	@Override
 	public Configuration configuration() {
@@ -39,12 +56,12 @@ public abstract class JBehaveJUnitTest extends JUnitStory {
 	}
 
 	/**
-	 * Returns a factory that provides a configuration (above) and one or more
-	 * steps files. 
+	 * Returns a factory that provides a configuration (above) and one or more steps files. 
 	 */
 	@Override
 	public InjectableStepsFactory stepsFactory() {
 		
+		// Ask the subclass for the candidate steps object--we're assuming the subclass *is* the candidate steps class.
 		Object steps = getStepsClass();
 		
 		if (steps == null)
